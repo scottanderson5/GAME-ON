9389d10cea355129ad9447bd8c85bcb33aa5f27a
Scott <scottanderson5@gmail.com>
2016-05-12 14:17:26 -0500

    Adding before Merge

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,21 +1,22 @@
 
 //Slide toggle function for the nav in the header.
 $('.header-menu').click(function(){
+  //What is appearing and how fast
   $('.header-nav').slideToggle(500);
 });
 
 
-// the selector will match all input controls of type :checkbox
-// and attach a click event handler 
+// The selector will match all input controls of type :checkbox on review and request pages
+// And attach a click event handler 
 $("input:checkbox").on('click', function() {
-  // in the handler, 'this' refers to the box clicked on
+  // In the handler, 'this' refers to the box clicked on
   var $box = $(this);
   if ($box.is(":checked")) {
-    // the name of the box is retrieved using the .attr() method
-    // as it is assumed and expected to be immutable
+    //Tthe name of the box is retrieved using the .attr() method
+    // As it is assumed and expected to be immutable
     var group = "input:checkbox[name='" + $box.attr("name") + "']";
-    // the checked state of the group/box on the other hand will change
-    // and the current value is retrieved using .prop() method
+    // The checked state of the group/box on the other hand will change
+    // And the current value is retrieved using .prop() method
     $(group).prop("checked", false);
     $box.prop("checked", true);
   } else {
