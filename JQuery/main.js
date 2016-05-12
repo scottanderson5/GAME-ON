0afa23e5db9c264ddfd16d071c7db79f28e75727
Scott <scottanderson5@gmail.com>
2016-05-12 14:11:23 -0500

    Added JQUERY to allow only one selectionon checbox input

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,4 +1,26 @@
+
+//Slide toggle function for the nav in the header.
 $('.header-menu').click(function(){
   $('.header-nav').slideToggle(500);
 });
+
+
+// the selector will match all input controls of type :checkbox
+// and attach a click event handler 
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
+
 
