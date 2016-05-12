9a7e3fab9a156230c848f23335acc54308d5df84
Scott <scottanderson5@gmail.com>
2016-05-12 16:22:23 -0500

    JQuery Modal Script Added and Functional

--- JQuery/main.js
+++ JQuery/main.js
@@ -6,6 +6,28 @@ $('.header-menu').click(function(){
 });
 
 
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
+
+
+
+
 // The selector will match all input controls of type :checkbox on review and request pages
 // And attach a click event handler 
 $("input:checkbox").on('click', function() {
@@ -23,5 +45,43 @@ $("input:checkbox").on('click', function() {
     $box.prop("checked", false);
   }
 });
+
+
+//MODAL
+
+$(function(){
+
+var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
+
+  $('a[data-modal-id]').click(function(e) {
+    e.preventDefault();
+    $("body").append(appendthis);
+    $(".modal-overlay").fadeTo(500, 0.7);
+    //$(".js-modalbox").fadeIn(500);
+    var modalBox = $(this).attr('data-modal-id');
+    $('#'+modalBox).fadeIn($(this).data());
+  });  
+  
+  
+$(".js-modal-close, .modal-overlay").click(function() {
+  $(".modal-box, .modal-overlay").fadeOut(500, function() {
+    $(".modal-overlay").remove();
+  });
+});
+ 
+$(window).resize(function() {
+  $(".modal-box").css({
+    top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+    left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+  });
+});
+ 
+$(window).resize();
+ 
+});
+
+
+
+
 
 
