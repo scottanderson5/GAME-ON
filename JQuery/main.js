3dae9de8a509e7f7f33f8eb6aa755d6853c177df
Scott <scottanderson5@gmail.com>
2016-05-15 15:00:37 -0500

    Added Pop Up Modals to Home Page

--- JQuery/main.js
+++ JQuery/main.js
@@ -245,6 +245,72 @@ $(window).resize();
  
 });
 
+//MODAL WORKS (7)
+
+$(function(){
+
+var appendthis =  ("<div class='modal-overlay5 js-modal-close'></div>");
+
+  $('a[data-modal-id]').click(function(e) {
+    e.preventDefault();
+    $("body").append(appendthis);
+    $(".modal-overlay5").fadeTo(500, 0.7);
+    //$(".js-modalbox").fadeIn(500);
+    var modalBox = $(this).attr('data-modal-id');
+    $('#'+modalBox).fadeIn($(this).data());
+  });  
+  
+  
+$(".js-modal-close, .modal-overlay5").click(function() {
+  $(".modal-box7, .modal-overlay5").fadeOut(500, function() {
+    $(".modal-overlay5").remove();
+  });
+});
+ 
+$(window).resize(function() {
+  $(".modal-box7").css({
+    top: ($(window).height() - $(".modal-box7").outerHeight()) / 2,
+    left: ($(window).width() - $(".modal-box7").outerWidth()) / 2
+  });
+});
+ 
+$(window).resize();
+ 
+});
+
+//MODAL WORKS (8)
+
+$(function(){
+
+var appendthis =  ("<div class='modal-overlay5 js-modal-close'></div>");
+
+  $('a[data-modal-id]').click(function(e) {
+    e.preventDefault();
+    $("body").append(appendthis);
+    $(".modal-overlay5").fadeTo(500, 0.7);
+    //$(".js-modalbox").fadeIn(500);
+    var modalBox = $(this).attr('data-modal-id');
+    $('#'+modalBox).fadeIn($(this).data());
+  });  
+  
+  
+$(".js-modal-close, .modal-overlay5").click(function() {
+  $(".modal-box8, .modal-overlay5").fadeOut(500, function() {
+    $(".modal-overlay5").remove();
+  });
+});
+ 
+$(window).resize(function() {
+  $(".modal-box8").css({
+    top: ($(window).height() - $(".modal-box8").outerHeight()) / 2,
+    left: ($(window).width() - $(".modal-box8").outerWidth()) / 2
+  });
+});
+ 
+$(window).resize();
+ 
+});
+
 
 
 //STARS
