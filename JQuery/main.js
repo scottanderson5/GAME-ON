05396058ae31877af9aed639167b37a4218bc5cb
Scott <scottanderson5@gmail.com>
2016-05-17 11:52:13 -0500

    Presentation Push

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,7 +1,6 @@
 
-//Slide toggle function for the nav in the header.
+//SLIDE MENU TOGGLE
 $('.header-menu').click(function(){
-  //What is appearing and how fast
   $('.header-nav').slideToggle(500);
 });
 
@@ -29,17 +28,11 @@ $('.owl-carousel').owlCarousel({
 
 
 
-// The selector will match all input controls of type :checkbox on review and request pages
-// And attach a click event handler 
+//CHECKBOXES
 $("input:checkbox").on('click', function() {
-  // In the handler, 'this' refers to the box clicked on
   var $box = $(this);
   if ($box.is(":checked")) {
-    //Tthe name of the box is retrieved using the .attr() method
-    // As it is assumed and expected to be immutable
     var group = "input:checkbox[name='" + $box.attr("name") + "']";
-    // The checked state of the group/box on the other hand will change
-    // And the current value is retrieved using .prop() method
     $(group).prop("checked", false);
     $box.prop("checked", true);
   } else {
